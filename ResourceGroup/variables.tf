7586c082fc1e6861a3d51af8c09ea4e3f18040f6
azsuniljoshi <azsuniljoshi7791@gmail.com>
2026-01-08 21:03:12 +0530

    Terraform script to create resource group

---
+++ ResourceGroup/variables.tf
@@ -0,0 +1,11 @@
+variable "subscription_id" {
+  default = "28982ea3-73ca-49fc-8033-956382e33a89"
+}
+
+variable "location" {
+  default = "East Asia"
+}
+
+variable "resource_group_name" {
+  default = "rg-myazapp-eastasia-dev-001"
+}
