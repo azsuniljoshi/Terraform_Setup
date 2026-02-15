44d0304af94fd6ca19c9a868a0feeac9faa51fae
azsuniljoshi <azsuniljoshi7791@gmail.com>
2026-02-15 17:57:00 +0530

    Terraform template change for MS SQL database resource

--- Database/main.tf
+++ Database/main.tf
@@ -1,22 +1,16 @@
 provider "azurerm" {
   features {}
-    subscription_id = "fab39a42-3122-457f-8903-c6e75c7ac237"
-}
-
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-myazapp-eastasia-dev-001"
-  location = "East Asia"
+   subscription_id = var.subscription_id
 }
 
 # SQL Server
 resource "azurerm_mssql_server" "sql_server" {
-  name                         = "sqlserver-azwebapp-00" 
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
+  name                         = var.sql_server_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
   version                      = "12.0"
-  administrator_login          = "sqluser"
-  administrator_login_password = "myuser@1234" 
+  administrator_login          = var.sql_server_username
+  administrator_login_password = var.sql_server_password
 
   identity {
     type = "SystemAssigned"
@@ -25,14 +19,14 @@ resource "azurerm_mssql_server" "sql_server" {
 
 # SQL Database
 resource "azurerm_mssql_database" "sql_db" {
-  name               = "sql-azwebapp-centraldindia-001"
+  name               = var.sql_server_dbname
   server_id          = azurerm_mssql_server.sql_server.id
   sku_name           = "Basic"
   collation          = "SQL_Latin1_General_CP1_CI_AS"
   max_size_gb        = 2
   zone_redundant     = false
-  auto_pause_delay_in_minutes = 60
-  min_capacity       = 0.5
+  # auto_pause_delay_in_minutes = 60
+  # min_capacity       = 0.5
 }
 
 # Allow Azure services to access the SQL Server
---
+++ Database/variable.tf
@@ -0,0 +1,30 @@
+variable "subscription_id" {
+  default = "28982ea3-73ca-49fc-8033-956382e33a89"
+}
+
+variable "location" {
+  default = "East Asia"
+}
+
+variable "resource_group_name" {
+  default = "rg-myazapp-eastasia-dev-001"
+}
+
+variable "sql_server_name" {
+  default =  "sqlserver-azwebapp-00" 
+}
+
+variable "sql_server_username" {
+  default = "sqluser"
+}
+
+
+variable "sql_server_password" {
+  default =  "myuser@1234" 
+}
+
+
+variable "sql_server_dbname" {
+  default =  "sql-demo001-dev" 
+}
+
